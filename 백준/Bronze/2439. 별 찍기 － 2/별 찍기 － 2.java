0ddfa59de144ea916597951df3d7dfacfcde8f91
박진성 <qkrwlstjd2@naver.com>
2024-03-06 21:00:22 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 132 ms, Memory: 14468 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        String str = "*";
+        String blank = " ";
+        for(int i = 1; i <= N; i++){
+            System.out.print(blank.repeat(N - i));
+            System.out.println(str.repeat(i));
+        }
+
+    }
+}
