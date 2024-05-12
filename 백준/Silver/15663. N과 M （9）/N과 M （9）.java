af1b2e3c783d0299335e7242f356a8c8d7f5f7bc
박진성 <qkrwlstjd2@naver.com>
2024-05-12 22:45:12 +0900

    [Silver II] Title: N과 M (9), Time: 192 ms, Memory: 19384 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+
+public class Main {
+
+    static int[] arr;
+    static int[] output;
+    static boolean[] visited;
+    static int M;
+    static int N;
+
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] inputs = br.readLine().split(" ");
+        N = Integer.parseInt(inputs[0]);
+        M = Integer.parseInt(inputs[1]);
+
+        arr = new int[N];
+        visited = new boolean[N];
+        output = new int[M];
+
+        String[] items = br.readLine().split(" ");
+        for (int i = 0; i < items.length; i++) {
+            arr[i] = Integer.parseInt(items[i]);
+        }
+        Arrays.sort(arr);
+
+        backtracking(0);
+        sb.deleteCharAt(sb.length() - 1);
+        System.out.print(sb);
+    }
+
+    public static void backtracking(int level) {
+
+        if (level == M) {
+            for (int j : output) {
+                sb.append(j).append(" ");
+            }
+            sb.deleteCharAt(sb.length() - 1);
+            sb.append("\n");
+            return;
+        }
+
+        int before = 0;
+        for (int i = 0; i < N; i++) {
+            if (!visited[i] && before != arr[i]) {
+                visited[i] = true;
+                output[level] = arr[i];
+                before = arr[i];
+                backtracking(level + 1);
+                visited[i] = false;
+            }
+        }
+
+    }
+}
